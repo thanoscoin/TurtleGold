aa18a1646317ad2a375b54ff37aa4f57f1167f0a
TheDevMinerTV <tobigames200@gmail.com>
2019-04-21 15:40:23 +0200

    I am fucking stupid, right?

--- src/CryptoNoteCore/Currency.h
+++ src/CryptoNoteCore/Currency.h
@@ -259,7 +259,6 @@ public:
   CurrencyBuilder& upgradeHeightV3(uint32_t val) { m_currency.m_upgradeHeightV3 = val; return *this; }
   CurrencyBuilder& upgradeHeightV4(uint32_t val) { m_currency.m_upgradeHeightV4 = val; return *this; }
   CurrencyBuilder& upgradeHeightV5(uint32_t val) { m_currency.m_upgradeHeightV5 = val; return *this; }
-  CurrencyBuilder& upgradeHeightV6(uint32_t val) { m_currency.m_upgradeHeightV6 = val; return *this; }
   CurrencyBuilder& upgradeVotingThreshold(unsigned int val);
   CurrencyBuilder& upgradeVotingWindow(uint32_t val) { m_currency.m_upgradeVotingWindow = val; return *this; }
   CurrencyBuilder& upgradeWindow(uint32_t val);
